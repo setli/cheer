0ad34dc3a3b1858312fca9580ef3d1373bfce3b4
Setli <Set_li@163.com>
2018-01-23 21:54:23 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -11,46 +11,55 @@
       <el-main>
         <div class="main">
           <div class="SignFlowHeader">
-              <div class="logo"> </div>
+            <div class="logo"></div>
+            <div class="info">简洁而不简单的物流工具</div>
           </div>
           <div class="my-tab-margin">
             <div class="SignContainer-inner">
-              <!-- 注册 -->
-              <div class="register-content">
+              <!-- 登录 -->
+              <div class="login-content" v-if="mode == 0">
                 <div class="my-input">
-                <input sytle="none" type="text"  placeholder="手机号" v-model="username">
+                  <input sytle="none" type="text" placeholder="手机号或邮箱" v-model="username">
                 </div>
                 <div class="my-input">
-                  <input sytle="none" type="text"  placeholder="输入6位短信验证吗" v-model="passport">
+                  <input sytle="none" type="text" placeholder="密码" v-model="passport">
+                </div>
+                <p>
+                  <a class="SignFlow-smsInputButton" href="#">手机验证码登录</a>
+                  <a class="SignFlow-fogetPSW" href="#">忘记密码</a>
+                </p>
+                <el-button type="primary" @click="submit">登录</el-button>
+              </div>
+              <!-- 注册 -->
+              <div class="register-content" v-if="mode == 1">
+                <div class="my-input">
+                  <input sytle="none" type="text" placeholder="手机号" v-model="username">
+                </div>
+                <div class="my-input">
+                  <input sytle="none" type="text" placeholder="输入6位短信验证吗" v-model="passport">
                   <a class="SignFlow-smsInputButton" href="#">获取短信验证吗</a>
                 </div>
                 <el-button type="primary" @click="submit">注册</el-button>
-                <p class="Register-declaration"><span>注册即代表你同意</span> <a href="#">《用户协议》</a></p>
+                <p class="Register-declaration">
+                  <span>注册即代表你同意</span>
+                  <a href="#">《用户协议》</a>
+                </p>
               </div>
-              <!-- 登录 -->
-            <div class="login-content">
-              <div class="my-input">
-                <input sytle="none" type="text"  placeholder="手机号或邮箱" v-model="username">
-              </div>
-              <div class="my-input">
-                <input sytle="none" type="text"  placeholder="密码" v-model="passport">
-              </div>
-              <p>
-                <a class="SignFlow-smsInputButton" href="#">手机验证码登录</a>
-                <a class="SignFlow-fogetPSW" href="#">忘记密码</a>
-              </p>
-              
-              <el-button type="primary" @click="submit">登录</el-button>
             </div>
-            </div>
-            
             <div class="goto-another">
-                <p class="goto-login"> <span>已有简运账号？</span> <a href="#" onclick="changeContent">点击登录</a> </p>
-                <p class="goto-register"> <span>没有简运账号？</span> <a href="#" onclick="changeContent">点击注册</a> </p>
-            </div> 
-          </div> 
-
-              
+              <p class="goto-register" v-if="mode == 0">
+                <span>没有简运账号？</span>
+                <a href="#" @click="mode=1">点击注册</a>
+              </p>
+              <p class="goto-login" v-if="mode == 1">
+                <span>已有简运账号？</span>
+                <a href="#" @click="mode=0">点击登录</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="download">
+          <el-button type="primary" plain>下载桌面版</el-button>
         </div>
       </el-main>
       <el-footer>
@@ -67,10 +76,10 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
+      mode: 0, //0:登录 1:注册
       activeName2: "first",
       username: "",
       passport: ""
@@ -78,10 +87,6 @@ export default {
   },
   methods: {
     //登录注册切换
-   changechangeContent(){
-
-   },
-
     handleClick(tab, event) {
       console.log(tab, event);
     },
@@ -94,118 +99,123 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-
 .login {
   position: absolute;
   width: 100%;
-  height: 100%; 
+  height: 100%;
   display: flex;
   justify-content: center;
   background-image: url(../../assets/images/bg-plane.jpg);
   background-repeat: no-repeat;
-  background-size: 100% 100%;
-  //用css画背景
-    .draw1{ 
-      background: rgb(217, 217, 240);
-      top:0;
-      left: 10%;
-      width: 20%; 
-      height: 15%;
-    }
-    .draw2{
-      border: 1px solid #fff;
-      top:2%;
-      left: 10%;
-      width: 20%;
-      height: 20%;
-    }
-    .draw3{
-      border-bottom: 100px solid #669; 
-      bottom:0;
-      left: 0;
-    }
-    .absolute{
-      position: absolute;
-    }
-    .shadow{
-      box-shadow: 1px 1px 6px #e4d6e4;
-    }  
-    .draw4{
-      border-top: 100px solid #669;
-      right:0;
-      width: 20%; 
-      height: 15%;
-    }
-    
-    //平行四边形
-      .parallelogram { 
-          -webkit-transform: skew(-20deg); 
-          -moz-transform: skew(-20deg); 
-          -o-transform: skew(-20deg); 
-          margin-left:20px; 
-      }
-    /*三角形*/
-      .triangle-up { 
-          width: 0;
-          height: 0; 
-          border-left: 50px solid transparent;
-          border-right: 50px solid transparent; 
-      }
-      .triangle-topright {
-          width: 0; 
-          height: 0;  
-          border-left: 100px solid transparent; 
-      }
-  //画完
+  background-size: 100% 100%; //用css画背景
+  .draw1 {
+    background: rgb(217, 217, 240);
+    top: 0;
+    left: 10%;
+    width: 20%;
+    height: 15%;
+  }
+  .draw2 {
+    border: 1px solid #fff;
+    top: 2%;
+    left: 10%;
+    width: 20%;
+    height: 20%;
+  }
+  .draw3 {
+    border-bottom: 100px solid #669;
+    bottom: 0;
+    left: 0;
+  }
+  .absolute {
+    position: absolute;
+  }
+  .shadow {
+    box-shadow: 1px 1px 6px #e4d6e4;
+  }
+  .draw4 {
+    border-top: 100px solid #669;
+    right: 0;
+    width: 20%;
+    height: 15%;
+  } //平行四边形
+  .parallelogram {
+    -webkit-transform: skew(-20deg);
+    -moz-transform: skew(-20deg);
+    -o-transform: skew(-20deg);
+    margin-left: 20px;
+  }
+  /*三角形*/
+  .triangle-up {
+    width: 0;
+    height: 0;
+    border-left: 50px solid transparent;
+    border-right: 50px solid transparent;
+  }
+  .triangle-topright {
+    width: 0;
+    height: 0;
+    border-left: 100px solid transparent;
+  } //画完
 }
 
 .el-container {
   margin: 0 auto;
   width: 100%;
-  flex: 1;
+  height: 100%;
   z-index: 999;
+  .el-main {
+    min-height: 688px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
   .main {
-    // margin-left: 60%;
     margin: 0 auto;
-    margin-top: 50px;
     background-color: #fff;
     box-shadow: #aaa;
-    width: 400px;
+    box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+    box-sizing: border-box;
+    width: 432px;
     button {
       width: 100%;
       margin-top: 20px;
       line-height: 1.5;
       font-size: 16px;
     }
-    .SignFlowHeader{
+    .SignFlowHeader {
       padding-top: 50px;
-      .logo{ 
+      text-align: center;
+      .logo {
         width: 194px;
         height: 50px;
         background-image: url("../../assets/images/cheer-logo.jpg");
         background-size: contain;
-        background-repeat: no-repeat;   
-        margin: 0 auto;     
+        background-repeat: no-repeat;
+        margin: 0 auto;
+      }
+      .info {
+        margin-top: 10px;
+        color: #666;
+        font-size: 19px;
       }
     }
-      
     .my-tab-margin {
-      text-align: center; 
-      .SignContainer-inner{
-          padding: 0 50px 80px 50px;
-          margin-top: 10px; 
-          .register-content{
-            display: none
-          }
-          .login-content{}
-      } 
+      text-align: center;
+      .SignContainer-inner {
+        padding: 0 50px 80px 50px;
+        margin-top: 10px;
+        .register-content {
+        }
+        .login-content {
+        }
+      }
       input {
+        color: #8590a6;
         border: none;
         outline: medium;
         margin: 5px 10px;
-        line-height: 2;
-        font-size: 14px;
+        font-size: 16px;
         width: 100%;
       }
       .my-input {
@@ -213,41 +223,38 @@ export default {
         border-bottom: 1px solid #ddd;
         padding: 10px 0;
         position: relative;
-        .SignFlow-smsInputButton{
+        .SignFlow-smsInputButton {
           position: absolute;
           right: 0;
           top: 19px;
         }
       }
     }
-    .SignFlow-smsInputButton{
+    .SignFlow-smsInputButton {
       line-height: 3;
       float: left;
     }
-    .SignFlow-fogetPSW{
+    .SignFlow-fogetPSW {
       line-height: 3;
       float: right;
       color: #888;
     }
-    .Register-declaration{
+    .Register-declaration {
       line-height: 3;
       text-align: left;
-      color:#888;
-      a{
-        color:#888;
+      color: #888;
+      a {
+        color: #888;
       }
     }
-    .goto-another{
-      background-color: #eee; 
+    .goto-another {
+      background-color: #eee;
       line-height: 3;
       text-align: center;
-      .goto-login{
+      font-size: 16px;
+      .goto-login {
       }
-      .goto-register{
-        display: none;
-      }
-      a{
-        color: #555;
+      .goto-register {
       }
     }
   }
@@ -256,17 +263,28 @@ export default {
     position: static;
     bottom: 0;
     width: 100%;
-    color: #333;
     text-align: center;
     line-height: 2;
     display: block;
+    font-size: 12px;
+    color: #8590a6;
     span {
       border-right: 1px solid #ddd;
       padding: 0 10px;
     }
     .my-last-span {
-      border: none; 
+      border: none;
     }
+  }
+}
+.download {
+  padding: 15px;
+  width: 432px;
+  margin: 0 auto;
+  button {
+    width: 100%;
+    line-height: 25px;
+    font-size: 14px;
   }
 }
 </style>
